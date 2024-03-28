48640c502cbaca91b52b1e00ecc43459de819ec5
c-perl <c-perl@onu.edu>
2024-03-28 09:05:20 -0400

    String counter

--- Labratory8-1/Source.cpp
+++ Labratory8-1/Source.cpp
@@ -1,1 +1,27 @@
-#
+
+// getline.cpp - John K. Estell - 07 October 2011
+// demo of reading in a string from the keyboard
+#include <iostream>
+#include <string>
+using namespace std;
+
+int countCharacter(string str) {
+	int length = str.size();
+	return length; 
+
+}
+
+int main(void) {
+	string input;
+	while (true) {
+		cout << "Enter a string or Q to quit: ";
+		getline(cin, input);
+		if (input == "Q") break;
+		
+		int stringLength = countCharacter(input);
+		cout << stringLength << endl; 
+
+		
+	}
+	return 0;
+}
